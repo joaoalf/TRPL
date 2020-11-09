da45bba630026075b7032c63b24c8759409788b4
Joao Alfredo Gama Batista <ermitaoj@gmail.com>
2020-11-09 00:00:41 -0500

    Make unittest work.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -16,12 +16,40 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-	if args.len() < 3 {
-	    return Err("not enough arguments");
-	}
-	let query = args[1].clone();
-	let filename = args[2].clone();
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-	Ok(Config {query, filename})
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
